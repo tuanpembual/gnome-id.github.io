e6853b1030abfb0307de6bbbf43c06f80edfd8c3
sendz <sendzation@gmail.com>
2018-03-14 22:03:43 +0700

    Move hexo generate to gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,16 @@
 var gulp = require('gulp');
 var deploy = require('gulp-gh-pages');
+var exec = require('child_process').exec;
 
-gulp.task('deploy', function () {
+gulp.task('deploy', ['build-deploy'] , function () {
   return gulp.src('./public/**/*')
     .pipe(deploy({origin: 'origin', branch: 'master'}));
 });
+
+gulp.task('build-deploy', function(cb) {
+  exec('hexo generate', function(err, stdOut, stdErr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  })
+})
