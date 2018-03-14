50f87da3b0754a3c3e0ad465ce5a4d6df4c61a93
sendz <sendzation@gmail.com>
2018-03-14 23:16:50 +0700

    Use gulp deploy again

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var deploy = require('gulp-gh-pages');
 var exec = require('child_process').exec;
+var Hexo = require('hexo');
+var hexo = new Hexo(process.cwd(), {});
 
 gulp.task('deploy', ['build-deploy'] , function () {
   return gulp.src('./public/**/*')
@@ -8,5 +10,8 @@ gulp.task('deploy', ['build-deploy'] , function () {
 });
 
 gulp.task('build-deploy', function() {
-  exec('hexo generate')
+  hexo.init();
+  hexo.call('generate', {}, function() {
+    console.log('Generating Hexo Files');
+  });
 });
